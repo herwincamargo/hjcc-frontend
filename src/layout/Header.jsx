a43b37f1383abf143ebcd4ca6da61c5a9c5d55a5
herwincamargo <herwincamargo@gmail.com>
2025-04-19 11:58:18 -0500

    Actualización de backend: Agregado modelo de solicitud con slug SEO-friendly y ordenación por fecha

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark"> 
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark"> {/* Navbar dentro del componente Header */}
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">HJCC - Smart Services</Link>
         <button

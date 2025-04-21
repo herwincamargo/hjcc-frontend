26b581beaa586cf8e01687ab0d43d9757a424ac6
herwincamargo <herwincamargo@gmail.com>
2025-04-21 09:23:14 -0500

    configuración single urls

--- src/pages/SolicitudDetalles.jsx
+++ src/pages/SolicitudDetalles.jsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom"; // Usar useParams para obtener el slug
+import { useParams } from "react-router-dom"; // Para obtener el slug de la URL
 
 const SolicitudDetalles = () => {
-  const { slug } = useParams(); // Obtener el slug de la URL
+  const { slug } = useParams();  // Obtener el slug de la URL
   const [solicitud, setSolicitud] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    // Realizar la solicitud al backend usando el slug
     fetch(`https://hjcc-backend.onrender.com/api/solicitudes/${slug}`)
       .then((response) => response.json())
       .then((data) => setSolicitud(data))
       .catch((error) => setError("Solicitud no encontrada"));
-  }, [slug]);
+  }, [slug]);  // Se vuelve a ejecutar cada vez que cambie el slug
 
   if (error) {
     return <p className="text-center text-danger">{error}</p>;

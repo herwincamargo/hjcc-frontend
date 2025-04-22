0e24d6fca55d3cea4bb76631ef6545bed560c114
herwincamargo <herwincamargo@gmail.com>
2025-04-22 06:29:10 -0500

    Single URLs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
-import Header from "./layout/Header";  // Importar Header
-import Footer from "./layout/Footer";  // Importar Footer
-import Home from "./pages/Home";  // Página de solicitudes recientes
-import FormularioSolicitud from "./pages/FormularioSolicitud";  // Página de solicitud de servicio
-import Solicitudes from "./pages/Solicitudes";  // Página de mostrar todas las solicitudes
-import SolicitudDetalles from "./pages/SolicitudDetalles"; // Página para ver detalles de la solicitud
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Home from "./pages/Home";
+import FormularioSolicitud from "./pages/FormularioSolicitud";
+import Solicitudes from "./pages/Solicitudes";
+import SolicitudDetalles from "./pages/SolicitudDetalles";
 
 const App = () => {
   return (
     <Router>
-      <Header /> {/* Mostrar Header globalmente */}
+      <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -19,7 +19,7 @@ const App = () => {
           <Route path="/solicitudes/:slug" element={<SolicitudDetalles />} />
         </Routes>
       </main>
-      <Footer /> {/* Mostrar Footer globalmente */}
+      <Footer />
     </Router>
   );
 };

b1cccc2fb0e217971fa47238874473dcfa785c59
herwincamargo <herwincamargo@gmail.com>
2025-04-22 10:12:05 -0500

    Footer fix

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-dark text-white text-center py-3">
-      <p>&copy; 2025 HJCC Smart Services. Todos los derechos reservados.</p>
+      <p>
+        &copy; 2025 HJCC Smart Services. Todos los derechos reservados.{" "}
+        <a href="https://www.hjcc.com.co" className="text-white" target="_blank" rel="noopener noreferrer">
+          www.hjcc.com.co
+        </a>
+      </p>
     </footer>
   );
 };

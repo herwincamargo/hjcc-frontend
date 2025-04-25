08265fd60e8e6b031658c9488e5d01c224e8098c
herwincamargo <herwincamargo@gmail.com>
2025-04-24 22:52:59 -0500

    Floating telegram box

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom"; // Importar Link de React Router para redirigir
 
 const Header = () => {
+  const closeMenu = () => {
+    // Cierra el menú cuando se hace clic en un enlace
+    const navbarToggler = document.getElementById("navbarNav");
+    const bsCollapse = new bootstrap.Collapse(navbarToggler, {
+      toggle: false,
+    });
+    bsCollapse.hide(); // Cerrar el menú de la navbar
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -23,32 +32,49 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/"
+                  onClick={closeMenu} // Llamar a la función de cierre cuando se hace clic
+                >
                   <i className="fas fa-home"></i> Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/solicitar-servicio">
+                <Link
+                  className="nav-link"
+                  to="/solicitar-servicio"
+                  onClick={closeMenu}
+                >
                   <i className="fas fa-plus-circle"></i> Solicitar Servicio
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/solicitudes">
+                <Link
+                  className="nav-link"
+                  to="/solicitudes"
+                  onClick={closeMenu}
+                >
                   <i className="fas fa-list"></i> Solicitudes
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/registro-profesional">
+                <Link
+                  className="nav-link"
+                  to="/registro-profesional"
+                  onClick={closeMenu}
+                >
                   <i className="fas fa-briefcase"></i> Trabaja con Nosotros
                 </Link>
               </li>
-              {/* Agregado el enlace de Telegram */}
               <li className="nav-item">
                 <Link
                   className="nav-link"
                   to={{ pathname: "https://t.me/+KcLGOEqZaElhZmQx" }}
                   target="_blank"
                   rel="noopener noreferrer"
+                  onClick={closeMenu}
                 >
                   <i className="fab fa-telegram-plane"></i> Unirme a Telegram
                 </Link>
